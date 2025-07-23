e24f3da008ad52e813c39146009a23c4a1d08a8c
won428 <csw307110@gmail.com>
2025-07-23 14:26:03 +0900

    MyPrint 2nd commit

---
+++ src/ch01_variable_operator/Add.java
@@ -0,0 +1,27 @@
+package ch01_variable_operator;
+
+public class Add {
+    public static void main(String[] args) {
+        // 변수를 선언(정의)
+        int x; // 정수형 데이터를 위하여 변수 x 준비해 주세요.
+        int y;
+        int z;
+
+         x = 3; // x라는 곳에 숫자 3을 할당(대입)해주세요.(write)
+         y = 5;
+         z = x + y;
+         String message; // 문자열 데이터를 위하여 변수 메세지를 준비하세요.
+
+        // + 기호의 2가지 역할 : (1) 덧셈, (2) 문자열 결합
+        message = x + " 더하기 " + y + "는(은) " + z + "입니다";
+        System.out.println(message);
+
+        x = 4; // 3을 4로 치환
+        y = 8;
+        z = x * y;
+
+        message = x +" 곱하기 " + y + "은 " + z + "입니다";
+
+        System.out.println(message);
+    }
+}
--- src/ch01_variable_operator/MyPrint.java
+++ src/ch01_variable_operator/MyPrint.java
@@ -1,4 +1,27 @@
 package ch01_variable_operator;
 
+/*
+    작성자 : 홍길동
+    작성일 : 7월 23일
+    목적 : 처음 해보는 자바 프로그래밍
+ */
 public class MyPrint {
+    public static void main(String[] args) {
+        // 한줄 짜리 주석(comment)
+        System.out.println("홍"); // 성을 출력합니다.
+        System.out.println(); // 엔터를 누릅니다.
+        System.out.println("길"); // 길을 출력합니다.
+        System.out.println("동"); // 동을 출력합니다.
+
+        System.out.print("대한 민국");
+        System.out.println(" 화이팅");
+        System.out.println();
+        System.out.println("\"예체능\" 프로그램 메인 MC \'강호동\'");
+
+        System.out.println("이름\t국어\t영어\t수학");
+        System.out.println("김철수\t80\t70\t60");
+        System.out.println("박영희\t40\t50\t60");
+
+
+    }
 }

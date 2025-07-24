195d1dff95ff9639b9d7d7607b3c9deae2397ac7
won428 <csw307110@gmail.com>
2025-07-24 17:44:27 +0900

    MyJava 5th commit, 25.07.24

--- src/ch01_variable_operator/TernaryOperator.java
+++ src/ch01_variable_operator/TernaryOperator.java
@@ -27,17 +27,20 @@ public class TernaryOperator {
         result2 = y%x == 0 ? "yes" : "no";
         System.out.println("result : " + result2);
 
-        int su = 10;
+        int su = 15;
          // 숫자 7은(는) 홀수입니다.
         String message;
         message = su%2 != 0 ? "홀수" : "짝수";
         System.out.println("숫자 " + su + "은(는) " + message + "입니다.");
 
-        int score =  55;
-        message = score >= 60 ? "합격" : "불합격";
+        int score = 50;
+        String smart = score == 100 ? "만점" : "합격";
+        message = score >= 60 ? smart : "불합격";
+
         System.out.println(score + "점이므로 " + message + "입니다.");
-         // 60점 이상이면 합격.
-        //85점이므로 합격입니다.
+         // 60점 이상이면 합격, 100점이면 만점.
+         // 85점이므로 합격입니다.
+         // String에서 3개의 조건으로 숫자를 사용해야 할 때 숫자를 조건연산으로 치환해서 할당.
 
         x = 3;
         y = 8;
@@ -49,6 +52,7 @@ public class TernaryOperator {
         int min = x < y ? x : y;
         min = min < z ? min : z;
         System.out.println("min : " + min);
+         // 조건 연산을 누적해서 3개의 조건으로 사용.
 
 
 
